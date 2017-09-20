d92d43a3e97cac94d12fbaf32e942cedbbc32119
Javier Campos <guillermojaviercampo@gmail.com>
2017-09-20 19:38:17 -0300

    Frontend adaptado a .net

---
+++ NETPyme/Controllers/Info2Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NETPyme.Controllers
+{
+    public class Info2Controller : Controller
+    {
+        // GET: Info2
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
